f17b4e29b3aaabb88cc3ecaf7bbbdc3dd034ddab
takumi0505 <takumi.teshima0529@gmail.com>
2024-05-31 22:29:50 +0900

    Add User and Shift entities

---
+++ src/main/java/com/example/shiftscheduler/entity/User.java
@@ -0,0 +1,19 @@
+package com.example.shiftscheduler.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    // Getters and Setters
+}
+
